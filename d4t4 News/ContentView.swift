dfa3a727c688903d01ce98ec54ccaff07cb11f57
saifulomg <saifulomg@gmail.com>
2023-11-25 14:44:19 +0000

    Initial Commit

---
+++ d4t4 News/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  d4t4 News
+//
+//  Created by Briteyellow on 25/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ d4t4 News/d4t4_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  d4t4_NewsApp.swift
+//  d4t4 News
+//
+//  Created by Briteyellow on 25/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct d4t4_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
